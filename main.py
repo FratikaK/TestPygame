37b3a0907497c496e9c75a0ad43166ca14cf92aa
FratikaK <kamunyan17@gmail.com>
2021-10-05 11:48:15 +0900

    [add]image

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from player import Player
+
 pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
@@ -11,20 +13,24 @@ y = 60
 clock = pygame.time.Clock()
 clock.tick(60)
 
+player = Player("./img/player", 100, 100)
+player_image = pygame.image.load(player.get_player_image("mario.jpg"))
+screen.blit(player_image, player_image.get_rect())
+
 while not done:
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_DOWN]:
-        y += 1
+        player.add_location(0, 1)
     if pressed[pygame.K_UP]:
-        y -= 1
+        player.add_location(0, -1)
     if pressed[pygame.K_RIGHT]:
-        x += 1
+        player.add_location(1, 0)
     if pressed[pygame.K_LEFT]:
-        x -= 1
+        player.add_location(-1, 0)
     pygame.display.update()
     pygame.time.wait(10)
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(x, y, 90, 90))
+    screen.blit(player_image, (player.x_location, player.y_location))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
