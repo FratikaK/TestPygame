c1e10aaff37c1951f5e63cbb2d26da8200ed52cd
FratikaK <kamunyan17@gmail.com>
2021-10-06 12:15:53 +0900

    [fix]不要な処理を削除

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pygame
-
 from player import Player
 
+# 必ず初期化する
 pygame.init()
 
+# スクリーンサイズ
 screen = pygame.display.set_mode((500, 500))
+# ウィンドウの名前
 pygame.display.set_caption("Test Pygame")
+
+# Trueになったらゲームが終了する
 done = False
-x = 60
-y = 60
 
-clock = pygame.time.Clock()
-clock.tick(60)
-
+# player.pyから
 player = Player("./img/player", 100, 100)
+
+# 画像をpygameにロードさせる
 player_image = pygame.image.load(player.get_player_image("mario.jpg"))
 screen.blit(player_image, player_image.get_rect())
 
 while not done:
+    # どのボタンを押したか
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_DOWN]:
         player.add_location(0, 1)
@@ -27,12 +30,16 @@ while not done:
         player.add_location(1, 0)
     if pressed[pygame.K_LEFT]:
         player.add_location(-1, 0)
-    pygame.display.update()
-    pygame.time.wait(10)
-    screen.fill((0, 0, 0))
-    screen.blit(player_image, (player.x_location, player.y_location))
 
+    # スクリーンを一旦綺麗にする
+    screen.fill((0, 0, 0))
+
+    # 画像を表示
+    screen.blit(player_image, (player.x_location, player.y_location))
+    pygame.display.update()
+
+    # イベント（ボタンを押す、マウスクリックするなど）を取得
     for event in pygame.event.get():
+        # ウィンドウを閉じる？　ゲームをやめた場合
         if event.type == pygame.QUIT:
             done = True
-    pygame.display.flip()
--- player.py
+++ player.py
@@ -2,9 +2,11 @@ class Player:
 
     def __init__(self, player_image_folder: str, x_location: int, y_location: int):
         self.player_image_folder = player_image_folder
+        # 画像を貼る座標
         self.x_location = x_location
         self.y_location = y_location
 
+    # 画像の場所を取得する
     def get_player_image(self, image_name: str):
         return self.player_image_folder + "/" + image_name
 
